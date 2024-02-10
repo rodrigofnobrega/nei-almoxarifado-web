1b3cae8e0435d478469e6c5a438678369bd1a254
Caio2a7 <caiodanielfonseca@gmail.com>
2024-02-10 18:37:43 -0300

    feat: adicionado manutenção de estado com pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias: {"@": "/<rootDir>"},
+  css: [
+    '~/assets/scss/styles.scss'
+  ],
+  modules: [
+    '@pinia/nuxt' 
+  ],
 })
