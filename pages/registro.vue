cf00ac970f1185cc15ac4f406476aa4a9b398e16
Caio2a7 <caiodanielfonseca@gmail.com>
2024-02-14 07:20:55 -0300

    feat: adicionado header nos componentes

--- pages/registro.vue
+++ pages/registro.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-      
-    </div>
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown link
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <li><a class="dropdown-item" href="https://mail.google.com/mail/u/0/#inbox">Action</a></li>
+    <li><a class="dropdown-item" href="https://mail.google.com/mail/u/0/#inbox">Another action</a></li>
+    <li><a class="dropdown-item" href="https://mail.google.com/mail/u/0/#inbox">Something else here</a></li>
+  </ul>
+</div>
 </template>
