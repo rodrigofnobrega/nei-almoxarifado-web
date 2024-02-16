0f716e583c6f9ffccf65ae47663254c9fc233467
leirdan <uouleirdan@gmail.com>
2024-02-15 21:08:29 -0300

    feat: criação de footer

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,5 +1,38 @@
+<script setup> </script>
+
 <template>
-    <div>
-      
+
+<footer class="custom-footer border-top text-center text-lg-start">
+  <section id="info">
+    <div class="container text-center text-md-start mt-5">
+       <div class="row"> 
+         <div class="col-md-4 col-lg-3 col-xl-3"> 
+	   <h5 class="fw-bold"> Contatos </h5>
+	   <p> Telefone: +55 (84) 98768-4609 </p>
+           <p class="text-underline text-break"> almoxarifado957@gmail.com </p> 
+         </div>
+	<div class="col-md-7 col-lg-7 col-xl-8"> 
+	   <p> Núcleo de Educação da Infânica - CAp/UFRN </p>
+	   <p class="d-none d-md-block"> Anel Viário do Campus, s/n - Lagoa Nova, Natal - RN, 59072-970 </p>
+	</div>
+	<div class="d-none d-md-block col-md-1 col-lg-1 col-xl-1"> 
+	   <a href="/"> <IconsLogo width="63" height="64" /> </a>
+	</div>
+       </div>
     </div>
+  </section>
+</footer>
+
 </template>
+
+<style> 
+
+.custom-footer { 
+  background-color: #0b3b69;
+  width: 100%;
+  bottom: 0;
+  height: 13rem;
+  position: absolute;
+  color: white;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
+  <div id="content">
     <Header />
+    <div id="content-wrap">
+    </div>
     <slot />
+    <Footer />
   </div>
 </template>
 
+<style> 
+  #content { 
+    position: relative;
+    min-height: 100vh;
+  }
+
+  #content-wrap { 
+    padding-bottom: 13rem; 
+  }
+</style>
