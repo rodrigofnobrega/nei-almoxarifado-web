c37cbbf7675069de93177e1becd195af398455d7
Caio2a7 <caiodanielfonseca@gmail.com>
2024-02-25 04:35:41 -0300

    fix: resolvendo alguns dos problemas listados nas issues

---
+++ components/cards/Solicitation.vue
@@ -0,0 +1,86 @@
+<script>
+import Card from './Card.vue'
+export default {
+	components: { Card },
+	props: ['person', 'requestedAt', 'expirationDate', 'message', 'itemName', 'itemTagging', 'quantity', 'itemQuantity']
+}
+</script>
+
+<template>
+	<Card>
+		<template v-slot:header> <h5> Solicitação </h5> </template>
+		<template v-slot:default> 
+			<div class="row">
+				<div class="col-6">
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Solicitador </label>
+						<input readonly class="form-control" type="text" :value="person">
+					</div>	
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Data </label>
+						<input readonly class="form-control" :value="requestedAt"> 
+					</div>	
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Data prevista para devolução </label>
+						<input readonly class="form-control" :value="expirationDate"> <slot name="devolutionDate" /> 
+					</div>	
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Mensagem da solicitação </label>
+						<textarea readonly class="form-control" :value="message"> </textarea>
+					</div>	
+				</div>
+				<div class="col-6">
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Item solicitado </label>
+						<input readonly class="form-control" :value="itemName"> 
+					</div>	
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Tombamento </label>
+						<input readonly class="form-control" :value="itemTagging">
+					</div>	
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Quantidade Solicitada </label>
+						<input readonly class="form-control" :value="quantity"> 
+					</div>	
+					<div class="mb-3"> 
+						<label class="form-label fw-bold"> Quantidade Disponível </label>
+						<input readonly class="form-control" :value="itemQuantity"> 
+					</div>	
+				</div>
+			</div>
+			</template>
+		<template v-slot:footer>
+			<div class="row justify-content-evenly"> 
+				<button class="btn btn-danger col-2 mx-2" data-bs-toggle="modal" data-bs-target="#rejectModal"> Rejeitar </button>
+				<button class="btn btn-outline-success col-2 mx-2" data-bs-toggle="modal" data-bs-target="#acceptModal"> Aceitar </button>
+			</div>
+		</template>
+	</Card>
+
+<ModalActionConfirm id="acceptModal">
+	<template v-slot:title> Confirmar aceitação </template>
+	<template v-slot:text> 
+		<h6> Mensagem para a notificação </h6>
+		<textarea class="form-control"> </textarea>
+	 </template>
+	<template v-slot:buttons> 
+		<button class="btn btn-secondary mx-2"> Enviar </button>
+	</template>
+ </ModalActionConfirm>
+
+<ModalActionConfirm id="rejectModal">
+	<template v-slot:title> Confirmar rejeição </template>
+	<template v-slot:text> 
+		<h6> Mensagem para a notificação </h6>
+		<textarea class="form-control"> </textarea>
+	 </template>
+	<template v-slot:buttons> 
+		<button class="btn btn-secondary mx-2"> Enviar </button>
+	</template>
+ </ModalActionConfirm>
+
+</template>
+
+<style scoped>
+</style>
+
--- pages/controle-de-acesso.vue
+++ pages/controle-de-acesso.vue
@@ -1,28 +1,52 @@
+<script setup lang="ts"> 
+	const person = { name: 'Gabriel' };
+	const item = { name: 'Monitor Multilaser', quantity: 2, itemTagging: 2029103923 };
+	const requests = [
+	{ 
+		requestedAt: new Date(Date.now()).toLocaleDateString(),
+		expirationDate: new Date(Date.now()).toLocaleDateString(),
+		message: "Solicito o item para a sala A308",
+		quantity: 1,
+		person: person.name,
+		item: item.name,
+		itemTagging: item.itemTagging,
+		itemQuantity: item.quantity
+	 }, 
+	{
+		requestedAt: new Date(Date.now()).toLocaleDateString(),
+		expirationDate: new Date(Date.now()).toLocaleDateString(),
+		message: "Solicito o item para a sala B204",
+		quantity: 15,
+		person: "Clezio",
+		item: "Copo descartável",
+		itemTagging: 20191029301, 
+		itemQuantity: 200
+	},
+	]; 
+</script>
 <template>
-    <div class="container col-1 d-block" style="margin-left: 170px;">
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
-        <p>Controle de Acesso</p>
+    <div class="container d-flex content">
+	<div class="row">
+		<div v-for="req in requests" class="col-xl-6 col-12 mb-3 mb-xl-0"> 
+			<CardsSolicitation 
+				:person="req.person"
+				:requestedAt="req.requestedAt"
+				:expirationDate="req.expirationDate"
+				:message="req.message"
+				:itemName="req.item"
+				:itemTagging="req.itemTagging"
+				:quantity="req.quantity"
+				:itemQuantity="req.itemQuantity"
+			> 
+			</CardsSolicitation>
+		</div>
+	</div>
     </div>
 </template>
+
+<style scoped>
+	.content {
+		margin-left: 3vh;
+		margin-right: 3vh;
+	}
+ </style>
