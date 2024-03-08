167a037351823527ae0e22e16bb23ea65dc5c3a7
Caio2a7 <caiodanielfonseca@gmail.com>
2024-03-08 10:02:28 -0300

    feat: adicionado homepage de navegação para o inventário

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,12 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [ 
-        {rel: 'icon', type: 'svg', href: '/logo.svg'} 
+        {rel: 'icon', type: 'svg', href: '/logo.svg'},
+      ],
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Gerenciador de Almoxarifado' },
       ]
     }
   }
