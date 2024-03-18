edf26be8c9c71e03330800d6346b481cc01e7753
Caio2a7 <caiodanielfonseca@gmail.com>
2024-03-18 07:24:14 -0300

    feat: adicionado integração de dados no modal de histórico

--- components/modal/ItemHistory.vue
+++ components/modal/ItemHistory.vue
@@ -4,7 +4,25 @@
 			<h5 class="modal-title"> Histórico mais recente do item </h5>
 		</template>
 	    <template v-slot:body>
-			<TablesHistory :item_tb_history="item_history"/>
+			<table class="table text-center"> 
+		<thead>
+			<tr>
+				<th scope="col"> Data </th> 
+				<th scope="col"> Operação </th> 
+				<th scope="col"> Quantidade </th> 
+				<th scope="col"> Autor </th> 
+			</tr>
+		 </thead>
+		<tbody>
+			<tr v-for="history in item_history.history" :key="'Amauri'"> 
+				<td scope="row" class="p-2 mb-3"> {{ history }} </td>
+				<td class="fw-bolder p-2 mb-3" style="--bs-bg-opacity: .99"
+					:class="{'text-light bg-success border-0': isCreate(history), 'text-light bg-danger border-0': isDelete(history), 'text-light bg-warning border-0': isRequest(history)}"> {{ history.split('-')[1] }} </td>
+				<td > {{item_history.qtd}} </td>
+				<td > {{'Amauri'}} </td>
+			</tr>
+		</tbody>
+	</table>
 		</template>
 		<template v-slot:footer> 
 			<div class="container-fluid d-flex justify-content-center align-items-center">
@@ -21,7 +39,27 @@ export default{
 			type: Object,
 			required: true
 		}
-	}
+	},
+	methods:{
+	isCreate(data){
+		if(data.includes('Adicionado')){
+			return data; 
+		}
+	},
+	isRequest(data){
+		if(data.includes('Consumido')){
+			return data; 
+		}
+	},
+	isDelete(data){
+		if(data.includes('Excluido')){
+			return data; 
+		}
+	},
+  },
+  mounted(){
+	console.log(this.item_history.history.length);	
+  }
 }
 
 </script>
--- stores/storage.ts
+++ stores/storage.ts
@@ -3,11 +3,11 @@ import { defineStore } from "pinia";
 export const useStorageStore = defineStore('storage', {
     state: () => ({
       items: [
-        {name: "Cartolina Amarela", sipac: null, type: "Unidade", qtd: 5, history: ["03/03/2023 13:30:00 - Adicionado"], storage: "almoxarifado-escolar"},
+        {name: "Cartolina Amarela", sipac: null, type: "Unidade", qtd: 5, history: ["03/03/2023 13:30:00 - Adicionado", "03/03/2023 14:32:34 - Consumido"], storage: "almoxarifado-escolar"},
         {name: "Lapiseira", sipac: "2342342354", type: "Caixa", qtd: 7, history: ["03/03/2023 14:32:34 - Consumido"], storage: "almoxarifado-escolar"},
         {name: "Folha em Branco A4", sipac: "4442342354", type: "Sacola", qtd: 30, history: ["03/03/2023 12:01:23 - Consumido"], storage: "almoxarifado-escolar"},
-        {name: "Sabão em pó", sipac: "5442342354", type: "Unidade", qtd: 12, history: ["03/03/2023 11:23:30 - Solicitado"], storage: "almoxarifado-escolar"},
-        {name: "Esponja", sipac: "6442342354", type: "Sacola", qtd: 3, history: ["03/04/2023 16:00:00 - Solicitado"], storage: "almoxarifado-escolar"},
+        {name: "Sabão em pó", sipac: "5442342354", type: "Unidade", qtd: 12, history: ["03/03/2023 11:23:30 - Excluido"], storage: "almoxarifado-escolar"},
+        {name: "Esponja", sipac: "6442342354", type: "Sacola", qtd: 3, history: ["03/04/2023 16:00:00 - Excluido"], storage: "almoxarifado-escolar"},
         {name: "Vassoura", sipac: null, type: "Unidade", qtd: 5, history: ["03/03/2023 13:30:00 - Adicionado"], storage: "almoxarifado-funcionarios"},
     ],
     sidebarSublinks: [],
