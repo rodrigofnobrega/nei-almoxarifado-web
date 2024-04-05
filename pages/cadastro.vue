8e223e173136ce3b5a06ff58b7fae3fe67b5b46c
murilocost <muriloalimacosta@hotmail.com>
2024-04-05 19:57:43 -0300

    Feat: manutenção da página de login e do seu layout, e criação da nova página de cadastro; 1/2 concluiudo. Falta a página de cadastro.

---
+++ pages/cadastro.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        cadastro
+    </div>
+</template>
+
+<script>
+    definePageMeta({
+        layout: "login",
+    })
+</script>
+
+<!--
+Pagina criada para o intuito inicial de cadastro
+que por sua vez ainda há de ser feita a tela
+-->
