bc4d321f418a602cb51609ee9df46f74e6c5ab4e
Caio2a7 <caiodaneilfonseca@gmail.com>
2024-04-28 09:29:31 -0300

    fix: possivelmente resolução final

--- app.vue
+++ app.vue
@@ -6,3 +6,16 @@
   </div>
 </template>
 
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const itemModals = document.getElementsByClassName("modal-header");
+  for (let i = 1; i < itemModals.length; i++) {
+    itemModals[i].classList.add("small");
+  }
+});
+
+
+
+</script>
--- layouts/authentication.vue
+++ layouts/authentication.vue
@@ -8,7 +8,15 @@
 </script>
 
 <style scoped>
-.block{
-    background-color: red;
+.block {
+    width: 300px;
+    height: 400px;
+    border: 1px 0px 0px 0px;
+    opacity: 0px;
+    margin: 100px auto;
+    background-color: #f2f2f2;
+    justify-content: center;         
+    display: flex;  
+    border-radius: 15px;                         
 }
 </style>
