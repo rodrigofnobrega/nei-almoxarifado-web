c19c012ea5fc300a9cd6ea4f1b91128ad4314f51
Caio2a7 <caiodaneilfonseca@gmail.com>
2024-04-28 09:34:47 -0300

    fix: removido fundo branco

--- layouts/authentication.vue
+++ layouts/authentication.vue
@@ -8,6 +8,14 @@
 </script>
 
 <style scoped>
-
-
+.block {
+    width: 300px;
+    height: 400px;
+    border: 1px 0px 0px 0px;
+    opacity: 0px;
+    margin: 100px auto;
+    justify-content: center;         
+    display: flex;  
+    border-radius: 15px;                         
+}
 </style>
