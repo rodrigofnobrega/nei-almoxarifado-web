b4a62e4f0c1285f6232630db8068f4f9b96d6d9f
murilocost <muriloalimacosta@hotmail.com>
2024-05-10 01:24:45 -0300

    Feat: Inserção de estilo e introdução de lógica para verificação de token enviado par ao email do usuário. Carência da lógica imersa na API

--- pages/emailAuth.vue
+++ pages/emailAuth.vue
@@ -1,21 +1,24 @@
 <template>
-	<div class="container-fluid login-container d-flex  col-1 justify-content-center">
+	<div class="container-fluid auth-container d-flex  col-1 justify-content-center">
 		<div class="header">
 			<p class="texto"><strong>Recuperar Senha</strong></p>
 		</div>
-        <div class="auth">
-            <form class="login-form" @submit.prevent="submitForm">
-
-                <label for="email">Email:</label>
+            <form class="auth-form" @submit.prevent="submitForm">
+                
+                <div class="auth">
+                    <label for="email">Email:</label>
                 <input type="text" id="email" placeholder="Seu email" v-model="email" required>
 
-                <label for="password">Senha:</label>
-                <input type="password" id="password" placeholder="Sua senha" v-model="password" required>
-
-                <button type="submit">Entrar</button>
+                <label for="token">Código de verificação:</label>
+                <input type="text" id="token" placeholder="Seu código" v-model="newToken" required>
+                </div>
+                
+                <button v-if="!isSubmit" type="submit" @click="submitEmail">Enviar</button>
+                
+                <button v-else-if="isSubmit" type="submit" @click="goToSenhaAuth">Ir</button>
 
 		    </form>
-        </div>
+        
 		
 
 	</div>
@@ -27,16 +30,25 @@ definePageMeta({
 });
 
 import { ref, onMounted } from 'vue';
-import { useUser } from '../stores/user.ts';
 
-const userStore = useUser();
+// Váriaveis responsivas que armazenam os valores dos campos acima
 const email = ref('');
-const password = ref('');
+const token = ref('AJA3');
+const newToken = ref('');
 
+// Variáveis de verificação se foi mandado ou não o código e para verificar o código
+const isSubmit = ref(false);
+const isToken = ref(false);
 
-const submitForm = () => {
-	userStore.fetchData(password.value, email.value);
+// Métodos que vão fazer que a rotina aconteça
+const submitEmail = () => {
+
 }
+
+const goToSenhaAuth = () => {
+
+}
+
 
 </script>
 
@@ -45,11 +57,16 @@ const submitForm = () => {
 	padding: 0px;
 }
 
-.login-container{
+.auth-container{
 	border-radius: 15px;
 	height: 400px;
 	width: 325px;
 	flex-direction: column;
+}
+.auth {
+    padding-top: 45px;
+    padding-bottom: 48px;
+    justify-content: space-around;
 }
 
 .texto {
@@ -64,14 +81,14 @@ const submitForm = () => {
 	width: 100%;
 	max-width: 350px;
 	height: 65px;
-	border-radius: 15px;
+	border-radius: 15px 15px 0px 0px;
 	background-color: #0B3B69;
 	color: #ffff;
 	margin-top: 0px;
 	padding: 0px;
 }
 
-.login-form {
+.auth-form {
 	margin-top: 10px;
 	margin-left: 10px;
 	margin-right: 10px;
@@ -79,13 +96,12 @@ const submitForm = () => {
 
 }
 
-.login-form label {
+.auth-form label {
 	display: block;
 	margin-bottom: 5px;
 }
 
-.login-form input[type="text"],
-.login-form input[type="password"] {
+.auth-form input[type="text"] {
 	width: 100%;
 	padding: 8px;
 	border: 1px solid #ccc;
@@ -93,7 +109,8 @@ const submitForm = () => {
 	margin-bottom: 10px;
 }
 
-.login-form button {
+
+.auth-form button {
 	width: 100%;
 	padding: 11px;
 	background-color: #71DD67;
@@ -101,24 +118,5 @@ const submitForm = () => {
 	border: none;
 	border-radius: 5px;
 	cursor: pointer;
-}
-
-.info {
-	margin-top: 20px;
-	padding: 12px;
-	text-align: center;
-}
-
-.info a {
-	color: #007bff;
-	text-decoration: none;
-}
-
-.info a:hover {
-	text-decoration: underline;
-}
-
-.login-form button:hover {
-	background-color: #71DD90;
 }
 </style>
