6a0ea6c90f9a60945f1ca15f9ec1a57f695ed49e
Caio2a7 <caiodaneilfonseca@gmail.com>
2024-05-31 22:37:33 -0300

    fix: corrigido página de solicitações

--- pages/nei/solicitacoes.vue
+++ pages/nei/solicitacoes.vue
@@ -1,30 +1,7 @@
 <template>
-    <div id="carouselExampleIndicators" class="carousel slide">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="/almo.png" widht="50px" height="100" class="d-block w-100" alt="AAAAAAAAAAAA">
-            </div>
-            <div class="carousel-item">
-              <img src="/almo2.png" widht="50px" height="100" class="d-block w-100" alt="AAAAAAAAAAAA">
-            </div>
-            <div class="carousel-item">
-              <img src="..." class="d-block w-100" alt="AAAAAAAAAAAA">
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
+    <div>
+          TESTE
+    </div>
 </template>
 
 <script setup>
@@ -35,7 +12,4 @@ definePageMeta({
 </script>
 
 <style scoped>
-#carouselExampleIndicators{
-    width: 400px !important;
-}
 </style>
