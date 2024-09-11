ccd3de25243a539ea909fbd8a8f7ab1e40e1e6ae
Caio2a7 <caiodanielfonseca@gmail.com>
2024-09-11 10:01:13 -0300

    fix: url do servidor

--- composables/axios.ts
+++ composables/axios.ts
@@ -3,7 +3,7 @@ import { useUser } from '../stores/user';
 export const useApi = () => {
     const userStore = useUser()
     const api = axios.create({
-        baseURL: 'https://192.168.0.6:8080/api/v1/',
+        baseURL: 'https://almoxarifado.nei.ufrn.br:8080/api/v1/',
     });
     return api
 }
